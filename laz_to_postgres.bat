7c76eb2e90cc8e3e3711c275c4d41f507feeae6f
illprogramtheshitoutofyou <t.blokland.1998@gmail.com>
2022-11-01 11:16:10 +0100

    Added batch files, data and pipeline

---
+++ laz_to_postgres.bat
@@ -0,0 +1,4 @@
+@echo off
+@REM run it like: batch hostname port user password dbname area
+@REM .\test_batch.bat localhost 5432 postgres tom-1998 paasheuvel_bgt paasheuvel
+pdal pipeline load_paasheuvel_laz_data.json
